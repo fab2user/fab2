2fbfd10d5ddb8aa8bbaca13fbb31a8630031c391
Landry Soules <landry.soules@nowina.lu>
2018-01-08 11:19:54 +0100

    Fix git issues with persistence module

---
+++ cdb2-persistence/src/main/java/eu/cehj/cdb2/Cdb2PersistenceApplication.java
@@ -0,0 +1,12 @@
+package eu.cehj.cdb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cdb2PersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cdb2PersistenceApplication.class, args);
+	}
+}
