496c63df6644c1d8a7c808572c58e5d7a28caa7e
Landry Soules <landry.soules@nowina.lu>
2018-01-12 11:39:54 +0100

    Update create/drop sql scripts

---
+++ scripts/sql/cdb2-drop.sql
@@ -0,0 +1,6 @@
+SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE if exists admin_area_subdivision_major;
+DROP TABLE if exists admin_area_subdivision_middle;
+DROP TABLE if exists admin_area_subdivision_minor;
+DROP TABLE if exists municipality;
+SET FOREIGN_KEY_CHECKS=1;
