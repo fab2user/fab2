9a10b9206d4aa433411b9ac16731dd8ec9edba02
Landry Soules <landry.soules@nowina.lu>
2018-01-12 08:41:04 +0100

    Add @Service annotation

--- cdb2-backend/src/main/java/eu/cehj/cdb2/business/service/data/GeoDataPersistenceService.java
+++ cdb2-backend/src/main/java/eu/cehj/cdb2/business/service/data/GeoDataPersistenceService.java
@@ -3,6 +3,7 @@ package eu.cehj.cdb2.business.service.data;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import eu.cehj.cdb2.service.db.AdminAreaSubdivisionMajorService;
@@ -10,6 +11,7 @@ import eu.cehj.cdb2.service.db.AdminAreaSubdivisionMiddleService;
 import eu.cehj.cdb2.service.db.AdminAreaSubdivisionMinorService;
 import eu.cehj.cdb2.service.db.MunicipalityService;
 
+@Service
 public class GeoDataPersistenceService implements DataPersistenceService {
 
     @Autowired
--- cdb2-backend/src/main/java/eu/cehj/cdb2/service/db/AdminAreaSubdivisionMajorService.java
+++ cdb2-backend/src/main/java/eu/cehj/cdb2/service/db/AdminAreaSubdivisionMajorService.java
@@ -1,8 +1,11 @@
 package eu.cehj.cdb2.service.db;
 
+import org.springframework.stereotype.Service;
+
 import eu.cehj.cdb2.business.service.data.GeoDataStructure;
 import eu.cehj.cdb2.entity.AdminAreaSubdivisionMajor;
 
+@Service
 public interface AdminAreaSubdivisionMajorService extends BaseService<AdminAreaSubdivisionMajor, Long> {
 
     public void updateAreaFromStructure(GeoDataStructure structure);
