debe8ddb11c7f352274a0abc2f05338b664f2e6e
Landry Soules <landry.soules@nowina.lu>
2018-02-01 08:23:02 +0100

    Add Swagger support

--- cdb2-web/src/main/java/eu/cehj/cdb2/web/config/WebConfig.java
+++ cdb2-web/src/main/java/eu/cehj/cdb2/web/config/WebConfig.java
@@ -10,8 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebConfig  extends WebMvcConfigurerAdapter{
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        //	    registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
+        registry.addResourceHandler("swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
     }
 }
 
