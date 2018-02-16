847f7613aa0cbf8f59264ba04d0b44d20e3c3dce
Landry Soules <landry.soules@nowina.lu>
2018-02-16 17:58:11 +0100

    Remove useless line

--- cdb2-web/src/main/java/eu/cehj/cdb2/web/config/WebSecurityConfig.java
+++ cdb2-web/src/main/java/eu/cehj/cdb2/web/config/WebSecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -56,7 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 ).permitAll()
 
         .antMatchers("/api/**", "/user", "/logout", "/v2/**").authenticated()
-        .antMatchers(HttpMethod.OPTIONS,"/api/**").permitAll()
+        //        .antMatchers(HttpMethod.OPTIONS,"/api/**").permitAll()
         .and()
         .cors()
         .and()
