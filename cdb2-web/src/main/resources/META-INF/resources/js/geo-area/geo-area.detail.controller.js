59277fdcd75d6c94795812350d059270dc884a9f
Landry Soules <landry.soules@nowina.lu>
2018-03-13 10:10:58 +0100

    Fix ngTable issues
    Had to use 2.2.0 because previous or later versions wouldn't work, and
    install it manually since webjars messes up with versions above 1.0.0.
    Adding *all* filtered data now works.

--- cdb2-web/src/main/resources/META-INF/resources/js/geo-area/geo-area.detail.controller.js
+++ cdb2-web/src/main/resources/META-INF/resources/js/geo-area/geo-area.detail.controller.js
@@ -5,72 +5,83 @@
     .module('cdb2')
     .controller('GeoAreaDetailController', GeoAreaDetailController);
 
-  GeoAreaDetailController.$inject = ['$log','$translate','$uibModalInstance','toastr', 'area', 'MunicipalityAPIService', 'GeoAreaAPIService', 'NgTableParams', 'ngTableEventsChannel', 'lodash'];
+  GeoAreaDetailController.$inject = ['$log', '$translate', '$uibModalInstance', 'toastr', 'area', 'MunicipalityAPIService', 'GeoAreaAPIService', 'NgTableParams', 'ngTableEventsChannel', 'lodash'];
 
-  function GeoAreaDetailController($log,$translate,$uibModalInstance, toastr, area, MunicipalityAPIService, GeoAreaAPIService, NgTableParams, ngTableEventsChannel, lodash) {
+  function GeoAreaDetailController($log, $translate, $uibModalInstance, toastr, area, MunicipalityAPIService, GeoAreaAPIService, NgTableParams, ngTableEventsChannel, lodash) {
     var vm = this;
     vm.area = area;
     vm.citiesToDisplay = area.municipalities.slice() || [];
-    vm.tableParamsEdit = new NgTableParams({}, {dataset: vm.citiesToDisplay});
+    vm.tableParamsEdit = new NgTableParams({
+      name: 'chosenCities'
+    }, {
+      dataset: vm.citiesToDisplay
+    });
     vm.selectedForRemoval = [];
     vm.selectedForAddition = [];
+    vm.tableParams = [];
+    vm.filteredCities = [];
 
-    $log.debug('eventChannel', ngTableEventsChannel);
-  //   ngTableEventsChannel.onAfterDataFiltered(function(p, results) {
-  //     $log.debug('all filtered: ', results);
-  // }
-  // , this.tableParams); 
-
-    MunicipalityAPIService.getAll().$promise.then(function(success) {
+    MunicipalityAPIService.getAll().$promise.then(function (success) {
       vm.municipalities = success;
-      vm.tableParams = new NgTableParams({}, {dataset: success});
+      vm.tableParams = new NgTableParams({
+        name: 'allCities'
+      }, {
+        dataset: success
+      });
     });
 
-    vm.toggleRemoval = function(cityId){
+    ngTableEventsChannel.onAfterDataFiltered(function (filter, ngTable, filteredData) {
+      if (ngTable === vm.tableParams) {
+        vm.filteredCities = filteredData;
+      }
+    });
+
+    vm.toggleRemoval = function (cityId) {
       var position = vm.selectedForRemoval.indexOf(cityId);
-      if(position < 0){
+      if (position < 0) {
         vm.selectedForRemoval.push(cityId);
-      }else{
+      } else {
         vm.selectedForRemoval.splice(position, 1);
       }
     };
 
-    vm.toggleAddition = function(city){
+    vm.toggleAddition = function (city) {
       var position = vm.selectedForAddition.indexOf(city);
-      if(position < 0){
+      if (position < 0) {
         vm.selectedForAddition.push(city);
-      }else{
+      } else {
         vm.selectedForAddition.splice(position, 1);
       }
     };
 
     // Add  cities found to citiesToDisplay
     // empty selectedForAddition
-    // FIXME: Make sure we can't add a city already present in the area
-    vm.addSelected = function(){
+    vm.addSelected = function () {
       addCitiesToDisplay(vm.selectedForAddition);
       vm.tableParamsEdit.reload();
       vm.selectedForAddition = [];
     };
 
-    vm.removeSelected = function(){
+    vm.removeSelected = function () {
       removeCitiesToDisplay(vm.selectedForRemoval);
       vm.tableParamsEdit.reload();
       vm.selectedForRemoval = [];
     };
 
-    function addCitiesToDisplay(cities){
-      cities.forEach(function(city){
-        if(!lodash.find(vm.citiesToDisplay, {'id': city.id})){
+    function addCitiesToDisplay(cities) {
+      cities.forEach(function (city) {
+        if (!lodash.find(vm.citiesToDisplay, {
+            'id': city.id
+          })) {
           vm.citiesToDisplay.push(city);
           vm.area.zipCodes += ', ' + city.postalCode;
         }
       });
     }
 
-    function removeCitiesToDisplay(cities){
+    function removeCitiesToDisplay(cities) {
       var zipsToRemove = lodash.map(cities, 'postalCode');
-      lodash.remove(vm.citiesToDisplay, function(cityToDisplay){
+      lodash.remove(vm.citiesToDisplay, function (cityToDisplay) {
         return zipsToRemove.indexOf(cityToDisplay.postalCode) > -1;
       });
 
@@ -78,27 +89,27 @@
 
     }
 
-    vm.addFiltered = function(){
+    vm.addFiltered = function () {
       // TODO: Add confirm
-      $log.info('Filtered cities: ', vm.tableParams.filteredData);
-      vm.tableParams.filteredData.forEach(function(city){
+      $log.info('Filtered cities: ', vm.filteredCities);
+      vm.filteredCities.forEach(function (city) {
         vm.toggleAddition(city);
       });
       vm.addSelected();
     };
 
-    vm.removeFiltered = function(){
-      vm.tableParamsEdit.data.forEach(function(city){
+    vm.removeFiltered = function () {
+      vm.tableParamsEdit.data.forEach(function (city) {
         vm.toggleRemoval(city);
       });
       vm.removeSelected();
     };
 
-    function filterZipList(zipList, zips){
-      var cleanZips = lodash.map(zipList.split(','), function(zip){
+    function filterZipList(zipList, zips) {
+      var cleanZips = lodash.map(zipList.split(','), function (zip) {
         return zip.trim();
       });
-      zips.forEach(function(zip){
+      zips.forEach(function (zip) {
         cleanZips = lodash.without(cleanZips, zip);
       });
       return cleanZips.join(', ');
@@ -109,27 +120,27 @@
         toastr.error('Error', 'At least one geo area must be selected !');
         return;
       }
-      if(valid){
-        vm.submitted=true;
-      vm.area.municipalities = vm.citiesToDisplay;
-     GeoAreaAPIService
-       .save({},vm.area)
-       .$promise
-       .then(function () {
-         $uibModalInstance.close();
-         toastr.success($translate.instant("global.toastr.save.success"));
-       })
-       .catch(function (err) {
-         $log.error(err);
-         vm.errorsFromServer = $translate.instant(err.data.message);
-       })
-       .finally(function () {
-         vm.submitted = false;
-       });
+      if (valid) {
+        vm.submitted = true;
+        vm.area.municipalities = vm.citiesToDisplay;
+        GeoAreaAPIService
+          .save({}, vm.area)
+          .$promise
+          .then(function () {
+            $uibModalInstance.close();
+            toastr.success($translate.instant("global.toastr.save.success"));
+          })
+          .catch(function (err) {
+            $log.error(err);
+            vm.errorsFromServer = $translate.instant(err.data.message);
+          })
+          .finally(function () {
+            vm.submitted = false;
+          });
       }
-   };
+    };
 
-    vm.cancel = function(){
+    vm.cancel = function () {
       $uibModalInstance.close();
     };
   }
