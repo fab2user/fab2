e4302f22a4f42959e67dd92a9d344e4913cb5df9
Landry Soules <landry.soules@nowina.lu>
2018-03-20 14:16:17 +0100

    Fix pagination issue on search

--- cdb2-hub/src/main/resources/META-INF/resources/js/status/status.controller.js
+++ cdb2-hub/src/main/resources/META-INF/resources/js/status/status.controller.js
@@ -1,13 +1,25 @@
-(function () {
+(function() {
   'use strict';
 
-  angular
-    .module('hub')
-    .controller('StatusController', StatusController);
+  angular.module('hub').controller('StatusController', StatusController);
 
-  StatusController.$inject = ['$log', 'NgTableParams', 'lodash', 'StatusAPIService', 'countryList', 'statuses'];
+  StatusController.$inject = [
+    '$log',
+    'NgTableParams',
+    'lodash',
+    'StatusAPIService',
+    'countryList',
+    'statuses'
+  ];
 
-  function StatusController($log, NgTableParams, lodash, StatusAPIService, countryList, statuses) {
+  function StatusController(
+    $log,
+    NgTableParams,
+    lodash,
+    StatusAPIService,
+    countryList,
+    statuses
+  ) {
     var vm = this;
 
     vm.filters = {
@@ -18,48 +30,56 @@
 
     vm.searchParams = {};
 
-    vm.tableParams = new NgTableParams({
-      count: 10,
-      page: 1
-    }, {
-      getData: function (params) {
-        var sort = function (params) {
-          var sortParam = params.sorting();
-          if (sortParam != null) {
-            if (Object.keys(sortParam).length > 0) {
-              var k = Object.keys(sortParam)[0];
-              return k + ',' + sortParam[k];
+    vm.tableParams = new NgTableParams(
+      {
+        count: 10,
+        page: 1
+      },
+      {
+        getData: function(params) {
+          var sort = function(params) {
+            var sortParam = params.sorting();
+            if (sortParam != null) {
+              if (Object.keys(sortParam).length > 0) {
+                var k = Object.keys(sortParam)[0];
+                return k + ',' + sortParam[k];
+              }
             }
-          }
-          return '';
-        };
-        $log.debug('url', sort(params));
-        return StatusAPIService.search(lodash.assign({
-          page: params.page() - 1,
-          size: params.count(),
-          sort: sort(params)
-        }, vm.searchParams)).$promise.then(function (success) {
-          params.total(success.totalElements);
-          return success.content;
-        });
+            return '';
+          };
+          $log.debug('url', sort(params));
+          return StatusAPIService.search(
+            lodash.assign(
+              {
+                page: params.page() - 1,
+                size: params.count(),
+                sort: sort(params)
+              },
+              vm.searchParams
+            )
+          ).$promise.then(function(success) {
+            params.total(success.totalElements);
+            return success.content;
+          });
+        }
       }
-    });
-
+    );
 
     vm.calendar = {
       opened: false
     };
 
-    vm.openCalendar = function () {
+    vm.openCalendar = function() {
       vm.calendar.opened = true;
     };
 
-    vm.search = function () {
+    vm.search = function() {
       if (vm.filters.date) {
         delete vm.searchParams.dateBefore;
         delete vm.searchParams.dateAfter;
-        // var formattedDate = new Date(vm.filters.date.getTime() + vm.filters.date.getTimezoneOffset() * 60000).toJSON().substring(0, vm.filters.date.toJSON().indexOf('T'));
-        var formattedDate = vm.filters.date.toJSON().substring(0, vm.filters.date.toJSON().indexOf('T'));
+        var formattedDate = vm.filters.date
+          .toJSON()
+          .substring(0, vm.filters.date.toJSON().indexOf('T'));
         if (vm.filters.ba === 'before') {
           vm.searchParams.dateBefore = formattedDate;
         } else {
@@ -69,12 +89,20 @@
       vm.tableParams.reload();
     };
 
-    vm.clearSearch = function () {
+    vm.clearSearch = function() {
       vm.searchParams = {};
+      // We must reset pagination before new display !!
+      vm.tableParams.page(1);
+
       delete vm.filters.date;
       vm.tableParams.reload();
     };
 
+    //When clicking search button, we must first reset pagination, otherwise results can be messed up !
+    vm.newSearch = function() {
+      // We must reset pagination before new display !!
+      vm.tableParams.page(1);
+      vm.search();
+    };
   }
-
 })();
