9665d1e79cf18d460dca59a75605b3de8c563c0a
Landry Soules <landry.soules@nowina.lu>
2018-03-27 15:02:09 +0200

    Cosmetic changes on Hub
    Align design with what has already been changed in cdb.

--- cdb2-hub/src/main/resources/META-INF/resources/js/country/country.controller.js
+++ cdb2-hub/src/main/resources/META-INF/resources/js/country/country.controller.js
@@ -1,78 +1,111 @@
-(function () {
-    'use strict';
-  
-    angular
-      .module('hub')
-      .controller('CountryController', CountryController);
-  
-      CountryController.$inject = ['$log','$interval', '$http', '$rootScope', '$uibModal', '$translate', 'NgTableParams', 'toastr', 'CountryAPIService', 'SyncService', 'EVENT', 'SERVER', 'STATUS'];
-  
-    function CountryController($log, $interval, $http, $rootScope, $uibModal, $translate, NgTableParams, toastr, CountryAPIService, SyncService, EVENT, SERVER, STATUS) {
-      var vm = this;
-      fetchCountries();
+(function() {
+  'use strict';
 
-      vm.edit = function(country){
-        var modalInstance = $uibModal.open({
-          templateUrl: '/js/country/country.edit.html',
-          controller: 'CountryEditController as countryEditCtrl',
-          windowClass: 'modal-md',
-          backdrop: 'static',
-          resolve: {
-            country: country
-          }
-        });
-        modalInstance.result.then(function () {
-          fetchCountries();
-        });
-      };
+  angular.module('hub').controller('CountryController', CountryController);
 
-      vm.delete = function(country){
-        CountryAPIService.delete({id: country.id}).$promise.then(function(){
-          toastr.success($translate.instant('global.toastr.delete.success'));
-          fetchCountries();
-        });
-      };
+  CountryController.$inject = [
+    '$log',
+    '$interval',
+    '$http',
+    '$rootScope',
+    '$uibModal',
+    '$translate',
+    'NgTableParams',
+    'toastr',
+    'CountryAPIService',
+    'SyncService',
+    'EVENT',
+    'SERVER',
+    'STATUS'
+  ];
 
-      vm.sync = function(country){
-        $log.info('Sync started');
-        SyncService.sync(country).then(function(success){
-          toastr.info($translate.instant('global.export.inprogress'));
-          $rootScope.$broadcast(EVENT.XML_EXPORT, success.data);
-          // start polling
-          vm.startPolling(success.data.id);
-        });
-      };
+  function CountryController(
+    $log,
+    $interval,
+    $http,
+    $rootScope,
+    $uibModal,
+    $translate,
+    NgTableParams,
+    toastr,
+    CountryAPIService,
+    SyncService,
+    EVENT,
+    SERVER,
+    STATUS
+  ) {
+    var vm = this;
+    fetchCountries();
 
-      vm.polling = undefined;
+    vm.edit = function(country) {
+      var modalInstance = $uibModal.open({
+        templateUrl: '/js/country/country.edit.html',
+        controller: 'CountryEditController as countryEditCtrl',
+        windowClass: 'modal-md',
+        backdrop: 'static',
+        resolve: {
+          country: country
+        }
+      });
+      modalInstance.result.then(function() {
+        fetchCountries();
+      });
+    };
 
-      vm.startPolling = function(taskId){
-        if(angular.isDefined(vm.polling)) return;
-        vm.polling = $interval(function(){
-          $http.get(SERVER.API + '/task/' + taskId).then(function(success){
+    vm.delete = function(country) {
+      CountryAPIService.delete({ id: country.id }).$promise.then(function() {
+        toastr.success($translate.instant('global.toastr.delete.success'));
+        fetchCountries();
+      });
+    };
+
+    vm.sync = function(country) {
+      $log.info('Sync started');
+      SyncService.sync(country).then(function(success) {
+        toastr.info($translate.instant('global.export.inprogress'));
+        $rootScope.$broadcast(EVENT.XML_EXPORT, success.data);
+        // start polling
+        vm.startPolling(success.data.id);
+      });
+    };
+
+    vm.polling = undefined;
+
+    vm.startPolling = function(taskId) {
+      if (angular.isDefined(vm.polling)) return;
+      vm.polling = $interval(
+        function() {
+          $http.get(SERVER.API + '/task/' + taskId).then(function(success) {
             // Refresh status on the screen
             $rootScope.$broadcast(EVENT.XML_EXPORT, success.data);
-            if(success.data.status === STATUS.OK || success.data.status === STATUS.ERROR){
+            if (
+              success.data.status === STATUS.OK ||
+              success.data.status === STATUS.ERROR
+            ) {
               vm.endPolling();
             }
           });
-        }, 50000, 5);
-      };
+        },
+        50000,
+        5
+      );
+    };
 
-
-    vm.endPolling = function(){
+    vm.endPolling = function() {
       if (angular.isDefined(vm.polling)) {
         $interval.cancel(vm.polling);
         vm.polling = undefined;
       }
     };
 
-      function fetchCountries(){
-        CountryAPIService.getAll().$promise.then(
-          function(success){
-            vm.tableParams = new NgTableParams({}, {dataset: success});
-          }
-        );
-      }
+    vm.resetSearch = function() {
+      vm.tableParams.filter({});
+    };
+
+    function fetchCountries() {
+      CountryAPIService.getAll().$promise.then(function(success) {
+        vm.tableParams = new NgTableParams({}, { dataset: success });
+      });
     }
-  })();
-  
+  }
+})();
