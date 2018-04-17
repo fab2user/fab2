841ab9f3aa9a7a28bc16c99ae5239f14384924e2
Landry Soules <landry.soules@nowina.lu>
2018-04-17 14:21:54 +0200

    WIP Fix changes requested by client
    - Replace 'municipality' labels with 'city'
    - Add filter on zip code
    - Download xls template for bailiffs import

--- cdb2-web/src/main/resources/META-INF/resources/js/home/tos.controller.js
+++ cdb2-web/src/main/resources/META-INF/resources/js/home/tos.controller.js
@@ -3,13 +3,14 @@
 
   angular.module('cdb2').controller('TosController', TosController);
 
-  TosController.$inject = ['$log', '$uibModalInstance', '$cookies'];
+  TosController.$inject = ['$log', '$uibModalInstance', '$cookies', 'moment'];
 
-  function TosController($log, $uibModalInstance, $cookies) {
+  function TosController($log, $uibModalInstance, $cookies, moment) {
     var vm = this;
     vm.modalInstance = $uibModalInstance;
     vm.accept = function() {
-      $cookies.put('fab-tos', 'ok');
+      var expireDate = moment('2050-01-01').toDate();
+      $cookies.put('fab-tos', 'ok', { expires: expireDate });
       vm.modalInstance.close();
     };
   }
