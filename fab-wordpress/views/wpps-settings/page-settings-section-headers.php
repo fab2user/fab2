4eff9b542b1823474266a39f4ceb82145d03d3a7
Landry Soules <landry.soules@nowina.lu>
2018-05-02 17:17:09 +0200

    FABWP: WIP FAB search form
    - Search form is now functionnal.
    - Plugin architecture has been simplified.
    - Mini maps added on bailiff cards

--- fab-wordpress/views/wpps-settings/page-settings-section-headers.php
+++
@@ -1,9 +0,0 @@
-<?php if ('wpps_section-basic' == $section['id']) : ?>
-
-	<p>Main configuration values.</p>
-
-<?php elseif ('wpps_section-advanced' == $section['id']) : ?>
-
-	<p>Another example section introduction.</p>
-
-<?php endif; ?>
