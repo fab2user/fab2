f96be7b737786990c77fa7f632aa09cc5b25d494
Landry Soules <landry.soules@nowina.lu>
2018-06-04 15:50:06 +0200

    Belgian Web Service integration
    Take WS action url from COS object instead of props file.

--- cdb2-hub/src/main/java/eu/cehj/cdb2/hub/service/search/BelgiumSearchService.java
+++ cdb2-hub/src/main/java/eu/cehj/cdb2/hub/service/search/BelgiumSearchService.java
@@ -7,24 +7,34 @@ import java.util.stream.Collectors;
 
 import org.datacontract.schemas._2004._07.lnxnkcngdwhdjconsult.ArrayOfOffice;
 import org.datacontract.schemas._2004._07.lnxnkcngdwhdjconsult.Office;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.MultiValueMap;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
 
+import eu.cehj.cdb2.business.service.db.CountryOfSyncService;
 import eu.cehj.cdb2.common.dto.BailiffDTO;
+import eu.cehj.cdb2.common.exception.dto.CDBException;
+import eu.cehj.cdb2.entity.CountryOfSync;
 import https.www_nkcn_cia.FCBBGetOffices;
 import https.www_nkcn_cia.FCBBGetOfficesResponse;
 import https.www_nkcn_cia.ObjectFactory;
 
 public class BelgiumSearchService extends WebServiceGatewaySupport implements LocalWSSearchService {
 
-    @Value("${cdb.belgium.soap.action}")
-    private String soapAction;
+    @Autowired
+    private CountryOfSyncService cosService;
 
     @Override
     public List<BailiffDTO> sendQuery(final String countryCode, final MultiValueMap<String, String> params){
-
+        final CountryOfSync cos = this.cosService.getByCountryCode(countryCode);
+        if(cos == null) {
+            throw new CDBException(String.format("Unkown country code \"%s\"", countryCode));
+        }
+        final String soapAction = cos.getUrl();
+        if(soapAction == null) {
+            throw new CDBException("Web Service request impossible without Web Service Action URL.");
+        }
         final FCBBGetOffices req = new FCBBGetOffices();
         final String postalCode = params.getFirst("postalCode");
         final ObjectFactory factory = new ObjectFactory();
@@ -36,7 +46,7 @@ public class BelgiumSearchService extends WebServiceGatewaySupport implements Lo
             req.setStrOfficeName(factory.createFCBBGetOfficesStrOfficeName(name));
         }
         final FCBBGetOfficesResponse resp = (FCBBGetOfficesResponse) this.getWebServiceTemplate().marshalSendAndReceive(req,
-                new SoapActionCallback(this.soapAction));
+                new SoapActionCallback(soapAction));
         final ArrayOfOffice bailiffsContainer = resp.getFCBBGetOfficesResult().getValue();
         final List<Office> rawBailiffs = bailiffsContainer.getOffice();
         return rawBailiffs.stream().map(this::convertOfficeToDTO).collect(Collectors.toList());
