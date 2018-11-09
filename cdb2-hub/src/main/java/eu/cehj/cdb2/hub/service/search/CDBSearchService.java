81ff01aa5b74101e6d064b0081df91b5154f7c3f
lamiroda <daniel.lamiroy@nowina.lu>
2018-11-09 11:31:54 +0100

    integration of IT search WS and for the reverse CDB query.

--- cdb2-hub/src/main/java/eu/cehj/cdb2/hub/service/search/CDBSearchService.java
+++ cdb2-hub/src/main/java/eu/cehj/cdb2/hub/service/search/CDBSearchService.java
@@ -26,52 +26,52 @@ import eu.cehj.cdb2.entity.CountryOfSync.SearchType;
 @Service("cdbSearchService")
 public class CDBSearchService implements SearchService {
 
-    @Value("${cdb.instrument}")
-    private String instrument;
+	@Value("${cdb.instrument}")
+	private String instrument;
 
-    @Value("${cdb.competence.type}")
-    private String competenceType;
+	@Value("${cdb.competence.type}")
+	private String competenceType;
 
-    @Autowired
-    private CountryOfSyncService cosService;
+	@Autowired
+	private CountryOfSyncService cosService;
 
-    @Autowired
-    private BailiffService bailiffService;
+	@Autowired
+	private BailiffService bailiffService;
 
-    @Autowired
-    private RestTemplateBuilder builder;
+	@Autowired
+	private RestTemplateBuilder builder;
 
 
-    @Override
-    public List<BailiffDTO> sendQuery(final String countryCode, final MultiValueMap<String, String> params) {
-        final CountryOfSync cos = this.cosService.getByCountryCode(countryCode);
-        if(cos == null) {
-            throw new CDBException(String.format("Country code not found \"%s\".", countryCode));
-        }
-        if(cos.getSearchType() != SearchType.CDB) {
-            throw new CDBException(String.format("Search type \"%s\" not intended to be processed by CDBSearchService.",  cos.getSearchType()));
-        }
+	@Override
+	public List<BailiffDTO> sendQuery(final String countryCode, final MultiValueMap<String, String> params) {
+		final CountryOfSync cos = this.cosService.getByCountryCode(countryCode);
+		if(cos == null) {
+			throw new CDBException(String.format("Country code not found \"%s\".", countryCode));
+		}
+		if(cos.getSearchType() != SearchType.CDB) {
+			throw new CDBException(String.format("Search type \"%s\" not intended to be processed by CDBSearchService.",  cos.getSearchType()));
+		}
 
-        final String url = cos.getUrl();
-        if(url == null) {
-            throw new CDBException(String.format("Service url is unknown for country code \"%s\".", countryCode));
-        }
+		final String url = cos.getUrl();
+		if(url == null) {
+			throw new CDBException(String.format("Service url is unknown for country code \"%s\".", countryCode));
+		}
 
-        final RestTemplate restTemplate = this.builder.build();
+		final RestTemplate restTemplate = this.builder.build();
 
-        final MultiValueMap<String, String> headers =
-                new LinkedMultiValueMap<>();
-        headers.add("Content-Type", "application/json");
-        final Map<String,String> query = new LinkedHashMap<>();
-        query.put("country", countryCode);
-        query.put("courtPostalCode", params.getFirst("postalCode"));
-        query.put("instrument", this.instrument);
-        query.put("competenceType", this.competenceType);
-        final HttpEntity<Map<String, String>> request = new HttpEntity<>(query, headers);
-        final ResponseEntity<CDBResponse> response = restTemplate
-                .exchange(url, HttpMethod.POST, request, CDBResponse.class);
+		final MultiValueMap<String, String> headers =
+				new LinkedMultiValueMap<>();
+		headers.add("Content-Type", "application/json");
+		final Map<String,String> query = new LinkedHashMap<>();
+		query.put("country", countryCode);
+		query.put("postalCode", params.getFirst("postalCode"));
+		query.put("instrument", this.instrument);
+		query.put("competenceType", this.competenceType);
+		final HttpEntity<Map<String, String>> request = new HttpEntity<>(query, headers);
+		final ResponseEntity<CDBResponse> response = restTemplate
+				.exchange(url, HttpMethod.POST, request, CDBResponse.class);
 
-        return this.bailiffService.populateDTOsFromCDB(response.getBody());
-    }
+		return this.bailiffService.populateDTOsFromCDB(response.getBody());
+	}
 
 }
